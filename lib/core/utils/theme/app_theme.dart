193824890ffe3f24b75ca06663ef2e0a8523e27e
KenStarry <starrycodes@gmail.com>
2025-06-13 10:51:33 +0300

    Fonts and App Restructure

---
+++ lib/core/utils/theme/app_theme.dart
@@ -0,0 +1,189 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+class AppTheme {
+  static ThemeData lightTheme({Color? accent}) {
+    return ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+            brightness: Brightness.light,
+            seedColor: accent ?? primaryColor,
+            primary: accent ?? primaryColor,
+            onPrimary: scaffoldBackgroundSecondaryColor,
+            secondary: secondaryColor,
+            onSecondary: whiteColor,
+            tertiary: textBlack900,
+            onTertiary: whiteColor,
+            primaryContainer: primaryColor,
+            onPrimaryContainer: whiteColor,
+            error: redColor,
+            onError: whiteColor,
+            outline: accent ?? primaryColor),
+        primaryColor: primaryColor,
+        hintColor: textBlack600,
+        scaffoldBackgroundColor: scaffoldBackgroundColor,
+        useMaterial3: true,
+        appBarTheme: const AppBarTheme(
+          centerTitle: false,
+          backgroundColor: scaffoldBackgroundSecondaryColor,
+          iconTheme: IconThemeData(color: blackColor),
+          titleTextStyle: TextStyle(
+              color: blackColor, fontSize: 24.0, fontWeight: FontWeight.w700),
+        ),
+        // cardTheme: CardTheme(
+        //     margin: EdgeInsets.zero,
+        //     color: whiteColor,
+        //     elevation: 0.0,
+        //     shape: RoundedRectangleBorder(
+        //         borderRadius: BorderRadius.circular(10.0))),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          backgroundColor: whiteColor,
+          showSelectedLabels: false,
+          selectedItemColor: secondaryColor,
+        ),
+        bottomSheetTheme: BottomSheetThemeData(
+            dragHandleColor: textBlack600,
+            dragHandleSize: const Size(100, 3),
+            surfaceTintColor: whiteColor,
+            backgroundColor: whiteColor,
+            modalBackgroundColor: whiteColor),
+        snackBarTheme: SnackBarThemeData(
+            backgroundColor: accent ?? primaryColor,
+            contentTextStyle: const TextStyle(
+                color: whiteColor,
+                fontSize: 14.0,
+                fontWeight: FontWeight.w700)),
+        datePickerTheme: const DatePickerThemeData(
+            headerHeadlineStyle: TextStyle(
+                color: blackColor, fontSize: 22, fontWeight: FontWeight.w700),
+            weekdayStyle: TextStyle(
+                color: blackColor, fontSize: 14, fontWeight: FontWeight.w700),
+            dayStyle: TextStyle(
+                color: whiteColor, fontSize: 13, fontWeight: FontWeight.w700),
+            inputDecorationTheme: InputDecorationTheme(),
+            backgroundColor: whiteColor,
+            surfaceTintColor: whiteColor),
+        popupMenuTheme: const PopupMenuThemeData(
+            color: whiteColor, surfaceTintColor: whiteColor),
+        canvasColor: whiteColor,
+        dividerColor: Colors.transparent,
+        fontFamily: "Satoshi",
+        textTheme: AppTextTheme.lightTextTheme());
+  }
+
+  // static ThemeData darkTheme({bool isMidnight = true, Color? accent}) {
+  //   return ThemeData(
+  //       colorScheme: ColorScheme.fromSeed(
+  //           brightness: Brightness.dark,
+  //           seedColor: primaryColor,
+  //           primary: accent ?? primaryColor,
+  //           onPrimary: isMidnight ? whiteColorMidnight : (kIsWeb ? backgroundColorDark : whiteColorDark),
+  //           secondary: secondaryColor,
+  //           onSecondary: isMidnight ? whiteColorMidnight : whiteColorDark,
+  //           tertiary: whiteColor,
+  //           onTertiary: isMidnight ? whiteColorMidnight : whiteColorDark,
+  //           background:
+  //           isMidnight ? backgroundColorMidnight : backgroundColorDark,
+  //           onBackground: whiteColor,
+  //           error: redColor,
+  //           onError: whiteColor,
+  //           outline: accent ?? primaryColor),
+  //       primaryColor: primaryColor,
+  //       scaffoldBackgroundColor:
+  //       isMidnight ? backgroundColorMidnight : (kIsWeb ? whiteColorDark : backgroundColorDark),
+  //       hintColor: whiteColor.withOpacity(0.2),
+  //       // primaryColor: primaryColor,
+  //       useMaterial3: true,
+  //       appBarTheme: AppBarTheme(
+  //         backgroundColor: accent ?? primaryColor,
+  //         iconTheme: const IconThemeData(color: whiteColor),
+  //         titleTextStyle: const TextStyle(
+  //             color: whiteColor, fontSize: 24.0, fontWeight: FontWeight.w700),
+  //       ),
+  //       cardTheme: CardTheme(
+  //           margin: EdgeInsets.zero,
+  //           color: whiteColor,
+  //           elevation: 0.0,
+  //           shape: RoundedRectangleBorder(
+  //               borderRadius: BorderRadius.circular(10.0))),
+  //       bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+  //         backgroundColor: whiteColor,
+  //         showSelectedLabels: false,
+  //         unselectedItemColor: inactiveColor,
+  //         selectedItemColor: secondaryColor,
+  //       ),
+  //       datePickerTheme: DatePickerThemeData(
+  //         headerHeadlineStyle: const TextStyle(
+  //             color: whiteColor, fontSize: 22, fontWeight: FontWeight.w700),
+  //         weekdayStyle: const TextStyle(
+  //             color: whiteColor, fontSize: 14, fontWeight: FontWeight.w700),
+  //         dayStyle: const TextStyle(
+  //             color: whiteColor, fontSize: 13, fontWeight: FontWeight.w700),
+  //         inputDecorationTheme: const InputDecorationTheme(),
+  //         backgroundColor: isMidnight ? dialogBgMidnight : dialogBgDark,
+  //         surfaceTintColor: isMidnight ? dialogBgMidnight : dialogBgDark,
+  //       ),
+  //       canvasColor: isMidnight ? whiteColorMidnight : whiteColorDark,
+  //       bottomSheetTheme: BottomSheetThemeData(
+  //         dragHandleColor: whiteColor,
+  //         dragHandleSize: const Size(70, 3),
+  //         surfaceTintColor:
+  //         isMidnight ? whiteColorMidnightFaded : whiteColorDarkFaded,
+  //         backgroundColor:
+  //         isMidnight ? whiteColorMidnightFaded : whiteColorDarkFaded,
+  //         modalBackgroundColor:
+  //         isMidnight ? whiteColorMidnightFaded : whiteColorDarkFaded,
+  //       ),
+  //       snackBarTheme: SnackBarThemeData(
+  //           backgroundColor: accent ?? primaryColor,
+  //           contentTextStyle: const TextStyle(
+  //               color: whiteColor,
+  //               fontSize: 14.0,
+  //               fontWeight: FontWeight.w700)),
+  //       popupMenuTheme: PopupMenuThemeData(
+  //           color: isMidnight ? whiteColorMidnightFaded : whiteColorDarkFaded,
+  //           surfaceTintColor:
+  //           isMidnight ? whiteColorMidnightFaded : whiteColorDarkFaded),
+  //       dividerColor: Colors.transparent,
+  //       fontFamily: "Helvetica",
+  //       textTheme: AppTextTheme.darkTextTheme());
+  // }
+}
+
+class AppTextTheme {
+  static TextTheme lightTextTheme() {
+    return const TextTheme(
+      bodyLarge: TextStyle(
+          color: textBlack700, fontSize: 18, fontWeight: FontWeight.w600),
+      bodyMedium: TextStyle(
+          color: textBlack700, fontSize: 14, fontWeight: FontWeight.w500),
+      bodySmall: TextStyle(
+          color: textBlack700, fontSize: 12, fontWeight: FontWeight.w400),
+      titleLarge: TextStyle(
+          color: textBlack800, fontSize: 24, fontWeight: FontWeight.w700),
+      titleMedium: TextStyle(
+          color: textBlack800, fontSize: 22, fontWeight: FontWeight.w700),
+      titleSmall: TextStyle(
+          color: textBlack800, fontSize: 20, fontWeight: FontWeight.w700),
+    );
+  }
+
+  static TextTheme darkTextTheme() {
+    return const TextTheme(
+      bodyLarge: TextStyle(
+          color: whiteColor, fontSize: 18, fontWeight: FontWeight.w600),
+      bodyMedium: TextStyle(
+          color: whiteColor, fontSize: 14, fontWeight: FontWeight.w500),
+      bodySmall: TextStyle(
+          color: whiteColor, fontSize: 12, fontWeight: FontWeight.w400),
+      titleLarge: TextStyle(
+          color: whiteColor, fontSize: 24, fontWeight: FontWeight.w700),
+      titleMedium: TextStyle(
+          color: whiteColor, fontSize: 22, fontWeight: FontWeight.w700),
+      titleSmall: TextStyle(
+          color: whiteColor, fontSize: 20, fontWeight: FontWeight.w700),
+    );
+  }
+}
---
+++ lib/core/utils/theme/colors.dart
@@ -0,0 +1,25 @@
+import 'dart:ui';
+
+/// ACCENT COLORS
+const primaryColor = Color(0xFF1C3D50);
+const secondaryColor = Color(0xFFFF6F61);
+
+const scaffoldBackgroundColor = Color(0xFFF5F5F7);
+const scaffoldBackgroundSecondaryColor = Color(0xFFFFFFFF);
+
+/// TEXT COLORS
+const textBlack900 = Color(0xFF000000);
+const textBlack800 = Color(0xFF1C1C1C);
+const textBlack700 = Color(0xFF333333);
+const textBlack600 = Color(0xFF4D4D4D);
+
+/// TEXT COLORS - DARK MODE
+const textWhite900 = Color(0xFFFFFFFF);
+const textWhite800 = Color(0xFFE6E6E6);
+const textWhite700 = Color(0xFFCCCCCC);
+const textWhite600 = Color(0xFFB3B3B3);
+
+/// NEUTRAL COLORS
+const whiteColor = Color(0xFFFFFFFF);
+const blackColor = Color(0xFF000000);
+const redColor = Color(0xFFFF0000);
