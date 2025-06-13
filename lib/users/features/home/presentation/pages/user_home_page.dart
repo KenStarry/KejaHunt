fa49e9219d6de4ed42231c716eadc2f09089a812
KenStarry <starrycodes@gmail.com>
2025-06-13 15:10:55 +0300

    New Home Updates and implementations

--- lib/users/features/home/presentation/pages/user_home_page.dart
+++ lib/users/features/home/presentation/pages/user_home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:keja_hunt/core/utils/theme/colors.dart';
 
 class UserHomePage extends StatefulWidget {
   const UserHomePage({super.key});
@@ -16,18 +18,91 @@ class _UserHomePageState extends State<UserHomePage> {
       margin: const EdgeInsets.only(top: 24),
       padding: const EdgeInsets.only(left: 16),
       child: CustomScrollView(
-        physics: const AlwaysScrollableScrollPhysics(parent: BouncingScrollPhysics()),
+        physics: const AlwaysScrollableScrollPhysics(
+          parent: BouncingScrollPhysics(),
+        ),
         slivers: [
           SliverToBoxAdapter(
-            child: Text(
-              "Discover\nyour new home!",
-              style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                fontSize: 26
-              ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              spacing: 8,
+              children: [
+                Text(
+                  "Discover\nyour new home!",
+                  style: Theme.of(
+                    context,
+                  ).textTheme.titleLarge?.copyWith(fontSize: 26),
+                ),
+                Text(
+                  "Start hunting for that dream home today",
+                  style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                    fontWeight: FontWeight.w700
+                  ),
+                  textAlign: TextAlign.start,
+                ),
+              ],
             ),
           ),
 
-          SliverToBoxAdapter()
+          SliverToBoxAdapter(
+            child: Container(
+              width: double.infinity,
+              height: 80,
+              margin: const EdgeInsets.only(top: 24),
+              child: ListView.builder(
+                itemBuilder: (context, index) => Container(
+                  width: 80,
+                  height: 80,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 55,
+                        height: 55,
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).colorScheme.primary,
+                          shape: BoxShape.circle,
+                          // boxShadow: [
+                          //   BoxShadow(
+                          //     color: Colors.black.withValues(alpha: 0.1),
+                          //     blurRadius: 4,
+                          //     offset: const Offset(0, 2),
+                          //   ),
+                          // ],
+                        ),
+                        child: UnconstrainedBox(
+                          child: SvgPicture.asset(
+                            'assets/images/icons/house.svg',
+                            width: 26,
+                            height: 26,
+                            colorFilter: ColorFilter.mode(
+                              whiteColor,
+                              BlendMode.srcIn,
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      Text(
+                        "1 Bdr",
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                itemCount: 10,
+                scrollDirection: Axis.horizontal,
+                physics: const BouncingScrollPhysics(),
+              ),
+            ),
+          ),
         ],
       ),
     );
