e9067b9c4b8508d612cd8c8591b5e1eae1634fbc
KenStarry <starrycodes@gmail.com>
2025-06-14 13:52:35 +0300

    Added Multiple Icons and Icon sets

---
+++ lib/core/features/auth/presentation/components/auth_type_card_mini.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:keja_hunt/core/features/auth/presentation/model/auth_type_card_model.dart';
+import 'package:keja_hunt/core/utils/theme/colors.dart';
+
+class AuthTypeCardMini extends StatelessWidget {
+  final AuthTypeCardModel authTypeCardModel;
+
+  const AuthTypeCardMini({super.key, required this.authTypeCardModel});
+
+  @override
+  Widget build(BuildContext context) {
+    return UnconstrainedBox(
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 18, horizontal: 32),
+        decoration: BoxDecoration(
+          color: Theme.of(context).scaffoldBackgroundColor,
+          border: Border.all(color: grey200, width: 1),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Center(
+          child: SvgPicture.asset(
+            authTypeCardModel.imagePath,
+            width: 30,
+            height: 30,
+          ),
+        ),
+      ),
+    );
+  }
+}
