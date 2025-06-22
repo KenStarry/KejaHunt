8c475e5a48abdcf667d836c2fbbc2d7abd73815a
KenStarry <starrycodes@gmail.com>
2025-06-22 21:36:02 +0300

    Stepper Screen Updates and implementation

--- lib/core/features/agents/agent_unit_upload/presentation/components/stepper_screen.dart
+++ lib/core/features/agents/agent_unit_upload/presentation/components/stepper_screen.dart
@@ -25,55 +25,59 @@ class _StepperScreenState extends State<StepperScreen> {
       color: Theme.of(context).scaffoldBackgroundColor,
       child: Column(
         children: [
-          EasyStepper(
-            activeStep: activeIndex,
-            direction: Axis.horizontal,
-            enableStepTapping: false,
-            unreachedStepBackgroundColor: Theme.of(
-              context,
-            ).scaffoldBackgroundColor,
-            activeStepBackgroundColor: Theme.of(context).colorScheme.primary,
-            stepRadius: 20,
-            padding: EdgeInsets.zero,
-            showTitle: true,
-            showLoadingAnimation: false,
-            showStepBorder: false,
-            lineStyle: LineStyle(
-              lineSpace: 6,
-              lineType: LineType.normal,
-              lineThickness: 8,
-              lineLength: 80,
-              unreachedLineColor: grey200,
-              activeLineColor: Theme.of(context).colorScheme.primary,
-              // progress: 0.5,
-              // progressColor: Theme.of(context).colorScheme.primary,
-            ),
-            steps: widget.steppers.map((step) {
-              final isActive = activeIndex >= widget.steppers.indexOf(step);
+          SizedBox(
+            height: 90,
+            child: EasyStepper(
+              activeStep: activeIndex,
+              direction: Axis.horizontal,
+              stepShape: StepShape.circle,
+              enableStepTapping: false,
+              unreachedStepBackgroundColor: Theme.of(
+                context,
+              ).scaffoldBackgroundColor,
+              activeStepBackgroundColor: Theme.of(context).colorScheme.primary,
+              stepRadius: 20,
+              padding: EdgeInsets.zero,
+              showTitle: true,
+              showLoadingAnimation: false,
+              showStepBorder: false,
+              lineStyle: LineStyle(
+                lineSpace: 6,
+                lineType: LineType.normal,
+                lineThickness: 8,
+                lineLength: 80,
+                unreachedLineColor: grey200,
+                activeLineColor: Theme.of(context).colorScheme.primary,
+                // progress: 0.5,
+                // progressColor: Theme.of(context).colorScheme.primary,
+              ),
+              steps: widget.steppers.map((step) {
+                final isActive = activeIndex >= widget.steppers.indexOf(step);
 
-              return EasyStep(
-                customStep: SvgPicture.asset(
-                  isActive ? step.activeIcon : step.inactiveIcon,
-                  width: 24,
-                  height: 24,
-                  colorFilter: ColorFilter.mode(
-                    isActive ? whiteColor : grey500,
-                    BlendMode.srcIn,
+                return EasyStep(
+                  customStep: SvgPicture.asset(
+                    isActive ? step.activeIcon : step.inactiveIcon,
+                    width: 24,
+                    height: 24,
+                    colorFilter: ColorFilter.mode(
+                      isActive ? whiteColor : grey500,
+                      BlendMode.srcIn,
+                    ),
                   ),
-                ),
-                customTitle: Text(
-                  step.title,
-                  textAlign: TextAlign.center,
-                  maxLines: 1,
-                  style: Theme.of(context).textTheme.bodySmall?.copyWith(
-                    fontWeight: FontWeight.w700,
-                    color: isActive
-                        ? Theme.of(context).colorScheme.primary
-                        : grey500,
+                  customTitle: Text(
+                    step.title,
+                    textAlign: TextAlign.center,
+                    maxLines: 1,
+                    style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                      fontWeight: FontWeight.w700,
+                      color: isActive
+                          ? Theme.of(context).colorScheme.primary
+                          : grey500,
+                    ),
                   ),
-                ),
-              );
-            }).toList(),
+                );
+              }).toList(),
+            ),
           ),
           Expanded(
             child: SizedBox(
@@ -86,7 +90,6 @@ class _StepperScreenState extends State<StepperScreen> {
                 itemBuilder: (context, index) => Container(
                   width: double.infinity,
                   height: double.infinity,
-                  color: Colors.yellow,
                   child: widget.steppers[index].stepperScreen,
                 ),
                 itemCount: widget.steppers.length,
--- lib/core/features/agents/agent_unit_upload/presentation/components/unit_upload_images_section.dart
+++ lib/core/features/agents/agent_unit_upload/presentation/components/unit_upload_images_section.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:keja_hunt/core/data/repository/unit_repository.dart';
 import 'package:keja_hunt/core/di/locator.dart';
@@ -8,9 +9,10 @@ import 'package:keja_hunt/core/presentation/components/custom_network_image.dart
 import 'package:keja_hunt/core/utils/theme/colors.dart';
 import 'package:sliver_tools/sliver_tools.dart';
 
-class UnitUploadImagesSection extends StatefulWidget {
+import '../../../../../domain/models/unit_image_model.dart';
 
-  final Function(List<XFile>) onImagesPicked;
+class UnitUploadImagesSection extends StatefulWidget {
+  final Function(List<UnitImageModel>) onImagesPicked;
 
   const UnitUploadImagesSection({super.key, required this.onImagesPicked});
 
@@ -20,14 +22,18 @@ class UnitUploadImagesSection extends StatefulWidget {
 }
 
 class _UnitUploadImagesSectionState extends State<UnitUploadImagesSection> {
-  List<XFile> pickedImages = [];
+  // List<XFile> pickedImages = [];
+  List<UnitImageModel> pickedUnitImages = [];
+
+  List<String> imageLabels = ['Unlabelled'];
 
   @override
   Widget build(BuildContext context) {
-    return SliverPadding(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
-      sliver: MultiSliver(
-        children: [
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
+        slivers: [
           SliverToBoxAdapter(
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.center,
@@ -42,20 +48,25 @@ class _UnitUploadImagesSectionState extends State<UnitUploadImagesSection> {
                       final unitRepo = locator.get<UnitRepository>();
 
                       await unitRepo.pickMultipleImages().then((images) {
-                        final currentImages = [...pickedImages];
+                        final currentImages = [...pickedUnitImages];
 
                         for (XFile image in images) {
                           if (!(currentImages
-                              .map((current) => current.name)
+                              .map((current) => current.imageFile!.name)
                               .toList()
                               .contains(image.name))) {
-                            currentImages.add(image);
+                            currentImages.add(
+                              UnitImageModel(
+                                imageFile: image,
+                                imageTag: "Unlabelled",
+                              ),
+                            );
                           }
                         }
 
                         widget.onImagesPicked(currentImages);
                         setState(() {
-                          pickedImages = currentImages;
+                          pickedUnitImages = currentImages.toList();
                         });
                       });
                     },
@@ -74,37 +85,227 @@ class _UnitUploadImagesSectionState extends State<UnitUploadImagesSection> {
 
           SliverToBoxAdapter(child: SizedBox(height: 16)),
 
-          SliverToBoxAdapter(
-            child: Container(
-              width: double.infinity,
-              height: 170,
-              child: ListView.separated(
-                physics: const BouncingScrollPhysics(),
-                itemBuilder: (context, index) => Container(
-                  width: 210,
-                  height: double.infinity,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(24),
-                    color: Theme.of(context).colorScheme.onPrimary,
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(24),
-                    child: Image.file(
-                      File(pickedImages[index].path),
-                      width: double.infinity,
-                      height: double.infinity,
-                      fit: BoxFit.cover,
+          /// Organize the Children based on labels
+          ...pickedUnitImages
+              .map((unitModel) => unitModel.imageTag)
+              .toSet()
+              .map(
+                (label) => MultiSliver(
+                  children: [
+                    SliverToBoxAdapter(
+                      child: Row(
+                        spacing: 24,
+                        children: [
+                          Text(
+                            label,
+                            style: Theme.of(context).textTheme.bodyLarge
+                                ?.copyWith(fontWeight: FontWeight.w600),
+                          ),
+                          Expanded(
+                            child: Divider(thickness: 1, color: grey300),
+                          ),
+                          Container(
+                            width: 30,
+                            height: 30,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Theme.of(context).scaffoldBackgroundColor,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: grey300.withValues(alpha: 0.5),
+                                  offset: Offset(0, 8),
+                                  blurRadius: 15,
+                                ),
+                              ],
+                            ),
+                            child: Center(
+                              child: Text(
+                                pickedUnitImages
+                                    .where(
+                                      (unitImage) =>
+                                          unitImage.imageTag == label,
+                                    )
+                                    .length
+                                    .toString(),
+                                style: Theme.of(context).textTheme.bodySmall
+                                    ?.copyWith(fontWeight: FontWeight.w700),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
+                    SliverToBoxAdapter(child: SizedBox(height: 24)),
+                    SliverGrid(
+                      delegate: SliverChildBuilderDelegate(
+                        (context, index) {
+                          final currentUnitImage = pickedUnitImages
+                              .where((unitImage) => unitImage.imageTag == label)
+                              .toList()[index];
+                          return Container(
+                            width: 210,
+                            height: double.infinity,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(24),
+                              color: Theme.of(context).scaffoldBackgroundColor,
+                            ),
+                            child: Stack(
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Expanded(
+                                      child: AspectRatio(
+                                        aspectRatio: 1 / 1,
+                                        child: Stack(
+                                          children: [
+                                            Padding(
+                                              padding: const EdgeInsets.only(top: 8.0, right: 8),
+                                              child: ClipRRect(
+                                                borderRadius:
+                                                    BorderRadius.circular(24),
+                                                child: Image.file(
+                                                  File(
+                                                    currentUnitImage
+                                                        .imageFile!
+                                                        .path,
+                                                  ),
+                                                  width: double.infinity,
+                                                  height: double.infinity,
+                                                  fit: BoxFit.cover,
+                                                ),
+                                              ),
+                                            ),
+
+                                            Align(
+                                              alignment: Alignment.topRight,
+                                              child: Container(
+                                                width: 35,
+                                                height: 35,
+                                                decoration: BoxDecoration(
+                                                  shape: BoxShape.circle,
+                                                  color: Theme.of(
+                                                    context,
+                                                  ).scaffoldBackgroundColor,
+                                                ),
+                                                child: IconButton(
+                                                  onPressed: () {
+                                                    final currentImages = [
+                                                      ...pickedUnitImages,
+                                                    ];
+
+                                                    currentImages.removeWhere(
+                                                      (file) =>
+                                                          file
+                                                              .imageFile!
+                                                              .name ==
+                                                          currentUnitImage
+                                                              .imageFile!
+                                                              .name,
+                                                    );
+
+                                                    widget.onImagesPicked(
+                                                      currentImages,
+                                                    );
+                                                    setState(() {
+                                                      pickedUnitImages =
+                                                          currentImages;
+                                                    });
+                                                  },
+                                                  padding: EdgeInsets.zero,
+                                                  icon: SvgPicture.asset(
+                                                    "assets/images/icons/close_outlined.svg",
+                                                    width: 28,
+                                                    height: 28,
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                    UnconstrainedBox(
+                                      child: TextButton(
+                                        onPressed: () {
+                                          /// Add this Image to Kitchen
+                                          final currentImageUsingFileName =
+                                              pickedUnitImages.firstWhere(
+                                                (unitImage) =>
+                                                    unitImage.imageFile!.name ==
+                                                    currentUnitImage
+                                                        .imageFile!
+                                                        .name,
+                                              );
+
+                                          final actualIndex = pickedUnitImages
+                                              .indexOf(
+                                                currentImageUsingFileName,
+                                              );
+
+                                          setState(() {
+                                            pickedUnitImages[actualIndex] =
+                                                pickedUnitImages[actualIndex]
+                                                    .copyWith(
+                                                      imageTag: "Bedroom",
+                                                    );
+                                          });
+                                        },
+                                        child: Row(
+                                          spacing: 6,
+                                          children: [
+                                            SvgPicture.asset(
+                                              "assets/images/icons/edit_alt_outlined.svg",
+                                              width: 16,
+                                              height: 16,
+                                              colorFilter: ColorFilter.mode(
+                                                Theme.of(
+                                                  context,
+                                                ).colorScheme.primary,
+                                                BlendMode.srcIn,
+                                              ),
+                                            ),
+                                            Text(
+                                              label == 'Unlabelled' ? "Add Label" : label,
+                                              textAlign: TextAlign.center,
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .bodySmall
+                                                  ?.copyWith(
+                                                    color: Theme.of(
+                                                      context,
+                                                    ).colorScheme.primary,
+                                                    fontWeight: FontWeight.bold,
+                                                  ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                        childCount: pickedUnitImages
+                            .where((unitImage) => unitImage.imageTag == label)
+                            .length,
+                      ),
+                      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                        maxCrossAxisExtent: 170,
+                        mainAxisExtent: 200,
+                        crossAxisSpacing: 24,
+                        mainAxisSpacing: 24,
+                        childAspectRatio: 1,
+                      ),
+                    ),
+                  ],
                 ),
-                itemCount: pickedImages.length,
-                scrollDirection: Axis.horizontal,
-                separatorBuilder: (BuildContext context, int index) =>
-                    SizedBox(width: 24),
               ),
-            ),
-          ),
-          SliverToBoxAdapter(child: SizedBox(height: 16)),
+
+          SliverToBoxAdapter(child: SizedBox(height: 150)),
         ],
       ),
     );
