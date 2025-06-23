804c3038810c3abc5ec5f321b34cad25b15f4721
KenStarry <starrycodes@gmail.com>
2025-06-23 09:37:20 +0300

    Added Unit Feature Vertical

---
+++ lib/core/features/users/house_unit_detail/presentation/components/unit_feature_vertical.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:keja_hunt/core/domain/models/unit_feature_model.dart';
+import 'package:keja_hunt/core/utils/theme/colors.dart';
+
+class UnitFeatureVertical extends StatefulWidget {
+  final UnitFeatureModel featureModel;
+
+  const UnitFeatureVertical({super.key, required this.featureModel});
+
+  @override
+  State<UnitFeatureVertical> createState() => _UnitFeatureVerticalState();
+}
+
+class _UnitFeatureVerticalState extends State<UnitFeatureVertical> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(color: Colors.transparent),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        spacing: 8,
+        children: [
+          Container(
+            width: 45,
+            height: 45,
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: primaryColor100,
+            ),
+            child: SvgPicture.asset(widget.featureModel.featureIconPath,
+            width: 20,
+            height: 20,
+            colorFilter: ColorFilter.mode(Theme.of(context).colorScheme.primary, BlendMode.srcIn),),
+          ),
+
+          Text(
+            (widget.featureModel.featureDescription != null &&
+                    widget.featureModel.featureDescription!.isNotEmpty
+                ? widget.featureModel.featureName
+                : widget.featureModel.featureName),
+            style: Theme.of(
+              context,
+            ).textTheme.bodyMedium?.copyWith(fontWeight: FontWeight.w700),
+          ),
+        ],
+      ),
+    );
+  }
+}
