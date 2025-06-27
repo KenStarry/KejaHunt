41df8558a9ea16e9b5c7ec5ecd4afd40fc9c32d2
KenStarry <starrycodes@gmail.com>
2025-06-27 10:59:28 +0300

    Custom User Marker

---
+++ lib/core/features/users/map/presentation/components/custom_user_marker.dart
@@ -0,0 +1,70 @@
+
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:keja_hunt/core/features/users/map/presentation/components/triangle_clipper.dart';
+import 'package:keja_hunt/core/utils/theme/colors.dart';
+
+import '../../../../../presentation/components/avatar.dart';
+
+class CustomUserMarker extends StatelessWidget {
+  final Uint8List bytes;
+
+  const CustomUserMarker({super.key, required this.bytes});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 70,
+      height: 70,
+      child: Stack(
+        children: [
+          //  profile pic
+          Align(
+            alignment: AlignmentDirectional.center,
+            child: Container(
+              width: 50,
+              height: 50,
+              margin: const EdgeInsets.only(bottom: 10),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(color: primaryColor, width: 3),
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [primaryColor100, primaryColor],
+                ),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: Image.memory(
+                  bytes,
+                  width: double.infinity,
+                  height: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+
+          //  triangle cutout
+          Positioned(
+            bottom: 4,
+            left: 0,
+            right: 0,
+            child: Align(
+              alignment: AlignmentDirectional.bottomCenter,
+              child: Transform.flip(
+                flipY: true,
+                child: ClipPath(
+                  clipper: TriangleClipper(),
+                  child: Container(width: 16, height: 16, color: primaryColor),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
