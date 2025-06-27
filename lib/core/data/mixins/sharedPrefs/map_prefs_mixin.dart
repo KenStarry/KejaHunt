4e3b7d87b0aea8ebc9a981f048b1b5847726ebfe
KenStarry <starrycodes@gmail.com>
2025-06-27 13:00:40 +0300

    Map Prefs Mixin and Shared Prefs Repository implementation

---
+++ lib/core/data/mixins/sharedPrefs/map_prefs_mixin.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../../di/locator.dart';
+
+mixin MapPrefsMixin {
+  final sharedPrefs = locator.get<SharedPreferences>();
+
+  /// Store Preferred Style in Preferences
+  Future<void> setMapStyles() async {
+    final lightMapStyle = await rootBundle.loadString(
+      'assets/json/map_styles/${'light'}_mode_style_4.json',
+    );
+
+    final darkMapStyle = await rootBundle.loadString(
+      'assets/json/map_styles/dark_mode_style_4.json',
+    );
+
+    sharedPrefs.setString('light-map', lightMapStyle);
+    sharedPrefs.setString('dark-map', darkMapStyle);
+  }
+
+  Future<Map<String, dynamic>> getMapStyles() async {
+    final lightMap = sharedPrefs.getString("light-map");
+    final darkMap = sharedPrefs.getString("dark-map");
+
+    return {"light-map": lightMap, "dark-map": darkMap};
+  }
+}
---
+++ lib/core/data/repository/shared_prefs_repository.dart
@@ -0,0 +1,3 @@
+import 'package:keja_hunt/core/data/mixins/sharedPrefs/map_prefs_mixin.dart';
+
+class SharedPrefsRepository with MapPrefsMixin {}
