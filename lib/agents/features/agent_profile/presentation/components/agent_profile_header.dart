cb44821e492f276421eba4b5c1ba7be6d691935c
KenStarry <starrycodes@gmail.com>
2025-06-30 10:18:29 +0300

    Agent Profile Header updates

--- lib/agents/features/agent_profile/presentation/components/agent_profile_header.dart
+++ lib/agents/features/agent_profile/presentation/components/agent_profile_header.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:keja_hunt/core/presentation/components/custom_filled_button.dart';
 import 'package:sliver_tools/sliver_tools.dart';
 
 import '../../../../../core/features/users/dashboard/presentation/bloc/user_bloc.dart';
@@ -19,6 +20,7 @@ class _AgentProfileHeaderState extends State<AgentProfileHeader> {
     return BlocBuilder<UserBloc, UserState>(
       builder: (context, userState) {
         return Column(
+          spacing: 24,
           children: [
             Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -32,50 +34,108 @@ class _AgentProfileHeaderState extends State<AgentProfileHeader> {
                   size: Size(100, 100),
                 ),
 
-                userState is UserSuccess
-                    ? Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        spacing: 8,
-                        children: [
-                          Text(
-                            '${userState.user?.username}',
-                            style: Theme.of(context).textTheme.titleSmall,
-                          ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    userState is UserSuccess
+                        ? Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      spacing: 8,
+                      children: [
+                        Text(
+                          '${userState.user?.username}',
+                          style: Theme.of(context).textTheme.titleSmall,
+                        ),
 
-                          SvgPicture.asset(
-                            "assets/images/icons/certificate_check.svg",
-                            width: 24,
-                            height: 24,
-                            colorFilter: ColorFilter.mode(
-                              Theme.of(context).colorScheme.primary,
-                              BlendMode.srcIn,
-                            ),
+                        SvgPicture.asset(
+                          "assets/images/icons/certificate_check.svg",
+                          width: 24,
+                          height: 24,
+                          colorFilter: ColorFilter.mode(
+                            Theme.of(context).colorScheme.primary,
+                            BlendMode.srcIn,
                           ),
-                        ],
-                      )
-                    : SizedBox.shrink(),
+                        ),
+                      ],
+                    )
+                        : SizedBox.shrink(),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      spacing: 8,
+                      children: [
+                        Container(
+                          width: 5,
+                          height: 5,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.greenAccent
+                          ),
+                        ),
+                        Text(
+                          'Online',
+                          style: Theme.of(context).textTheme.bodySmall,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ],
             ),
 
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              spacing: 8,
-              children: [
-                Container(
-                  width: 5,
-                  height: 5,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.greenAccent
+            /// Action Buttons
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                spacing: 24,
+                children: [
+                  Expanded(
+                    child: CustomFilledButton(text: "Book Agent", onTap: () {}),
                   ),
-                ),
-                Text(
-                  'Online',
-                  style: Theme.of(context).textTheme.bodySmall,
-                ),
-              ],
+
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: SvgPicture.asset(
+                          "assets/images/icons/message_outlined.svg",
+                          width: 24,
+                          height: 24,
+                          colorFilter: ColorFilter.mode(
+                            Theme
+                                .of(context)
+                                .colorScheme
+                                .primary,
+                            BlendMode.srcIn,
+                          ),
+                        ),
+                      ),
+
+                      IconButton(
+                        onPressed: () {
+                          /// Make a Phone Call
+                        },
+                        icon: SvgPicture.asset(
+                          "assets/images/icons/call_outlined.svg",
+                          width: 24,
+                          height: 24,
+                          colorFilter: ColorFilter.mode(
+                            Theme
+                                .of(context)
+                                .colorScheme
+                                .primary,
+                            BlendMode.srcIn,
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ]
+              ),
             )
           ],
         );
